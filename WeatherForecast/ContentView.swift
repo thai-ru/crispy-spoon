016bb884db91c2668381b07986abb307d2f30e90
thai-ru <robinthairu34@gmail.com>
2024-04-10 03:26:05 +0300

    Basic view, font styles and custom icons and colors

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,16 +9,72 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            DayForecast(
+                Day: "Mon",
+                highTemp: 70,
+                lowTemp: 50,
+                isRainy: false
+            )
+            
+            DayForecast(
+                Day: "Tue",
+                highTemp: 68,
+                lowTemp: 42,
+                isRainy: true
+            )
+            
+            DayForecast(
+                Day: "Wed",
+                highTemp: 54,
+                lowTemp: 22,
+                isRainy: true
+            )
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let Day: String
+    let highTemp: Int
+    let lowTemp: Int
+    let isRainy: Bool
+    
+    var iconName: String {
+        if isRainy == true {
+            "cloud.rain.fill"
+        } else {
+            "sun.max.fill"
+        }
+    }
+    
+    var iconColor: Color {
+        if isRainy {
+            Color.cyan
+        } else {
+            Color.yellow
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text(Day)
+                .font(.title2)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColor)
+                .font(.title)
+            Text("High: \(highTemp)")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+            Text("Low: \(lowTemp)")
+                .font(.subheadline)
+                .fontWeight(.medium)
+                .foregroundStyle(.secondary)
+        }
+        .padding()
+    }
 }
